5cb18f927dc441121799772baabe45cdff4bd41d
adash333 <adash333@hotmail.com>
2018-06-24 00:01:41 +0900

    Create the first Quiz Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('slides') slides: any;
+
+  slideOptions: any;
+  flashCardFlipped: boolean = false;
+
+  constructor(
+    public navCtrl: NavController,
+    public dataService: DataProvider
+  ) {    
 
   }
+
+  ionViewDidLoad() {
+
+  }
+
+  selectAnswer() {
+    this.flashCardFlipped = true;
+  }
+
 
 }
