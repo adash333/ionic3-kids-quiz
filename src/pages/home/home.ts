92d26edaf258d5b46800f7345e2a87439a79a91a
adash333 <adash333@hotmail.com>
2018-06-24 23:27:45 +0900

    multiple questions  and score

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,12 @@ export class HomePage {
 
   @ViewChild('slides') slides: any;
 
+  hasAnswered: boolean = false;
+  score: number = 0;
+
   slideOptions: any;
-  flashCardFlipped: boolean = false;
+  //flashCardFlipped: boolean = false;
+  questions: any;
 
   constructor(
     public navCtrl: NavController,
@@ -21,12 +25,57 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
+    this.slides.lockSwipes(true);
 
+    this.dataService.load().then((data) => {
+      data.map((question) => {
+        let originalOrder = question.answers;
+        question.answers = this.randomizeAnswers(originalOrder);
+        return question;
+      });
+      this.questions = data;
+    });
   }
 
-  selectAnswer() {
-    this.flashCardFlipped = true;
+  nextSlide() {
+    this.slides.lockSwipes(false);
+    this.slides.slideNext();
+    this.slides.lockSwipes(true);
   }
 
+  selectAnswer(answer, question) {
+    this.hasAnswered = true;
+    answer.selected = true;
+    question.flashCardFlipped = true;
+
+    if(answer.correct) {
+      this.score++;
+    }
+
+    setTimeout(() => {
+      this.hasAnswered = false;
+      this.nextSlide();
+      answer.selected = false;
+      question.flashCardFlipped = false;
+    }, 3000);
+  }
+
+  randomizeAnswers(rawAnswers: any[]): any[] {
+    for(let i = rawAnswers.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = rawAnswers[i];
+      rawAnswers[i] = rawAnswers[j];
+      rawAnswers[j] = temp;
+    }
+
+    return rawAnswers;
+  }
+
+  restartQuiz() {
+    this.score = 0;
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(1, 1000);
+    this.slides.lockSwipes(true);
+  }
 
 }
--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,17 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the DataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DataProvider {
+
+  data: any;
 
   constructor(public http: HttpClient) {
     console.log('Hello DataProvider Provider');
   }
 
+  load() {
+
+    if(this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.http.get('assets/data/questions.json').subscribe(data => {
+        this.data = data.questions;
+        resolve(this.data);
+      });
+    });
+
+  }
 }
